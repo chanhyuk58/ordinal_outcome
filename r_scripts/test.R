2142c8ce6543e2733fdd2b249628b75b7fabeb74
Chanhyuk Park <chan.hyuk.58@gmail.com>
2025-04-07 12:10:40 -0500

    2025-04-07 12:10 test.R: dataframe..

--- r_scripts/test.R
+++ r_scripts/test.R
@@ -1,8 +1,10 @@
 library('tidyverse')
 
 a <- 1 + 1
+msg_df <- data.frame()
 msg <- paste0('This is the test ', a)
 print(msg)
+msg_df <- rbind(msg_df, msg)
 
-write_excel_csv(msg, file='msg.csv')
-write_excel_csv(msg, file='../data/msg.csv')
+write_excel_csv(msg_df, file='msg.csv')
+write_excel_csv(msg_df, file='../data/msg.csv')
