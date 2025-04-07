f46cd3bd1e6d217cfeed0d755925282fb888ef2c
Chanhyuk Park <chan.hyuk.58@gmail.com>
2025-04-07 13:37:36 -0500

    2025-04-07 13:37

--- r_scripts/test.R
+++ r_scripts/test.R
@@ -1,5 +1,5 @@
-library('tidyverse')
-path <- getwd()
+library('readr')
+# path <- getwd()
 
 a <- 1 + 1
 msg_df <- data.frame()
@@ -8,4 +8,4 @@ print(msg)
 msg_df <- rbind(msg_df, msg)
 
 write_excel_csv(msg_df, file='msg.csv')
-write_excel_csv(msg_df, file=paste0(path, '/data/msg.csv'))
+write_excel_csv(msg_df, file='../data/msg.csv')
